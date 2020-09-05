1b2e2ec0669bb3c441493fa45815bbfa2ca729c0
Code <Poetry>
2020-09-05 14:57:33 -0500

    Update Intro.rb

--- Intro.rb
+++ Intro.rb
@@ -1,10 +1,17 @@
+#Intro
+
 
 use_synth :beep
 use_bpm 30
-sample :vinyl_hiss
+
+2.times do
+  sample :vinyl_hiss
+end
+
 sleep 2
-live_loop :left do
-  with_fx :ping_pong do
+
+with_fx :ping_pong do
+  live_loop :left do
     play 62
     sleep 1.5
     play 65
@@ -15,9 +22,30 @@ live_loop :left do
     sleep 1.5
   end
 end
-live_loop :right do
-  with_fx :ping_pong do
+
+sleep 6
+with_fx :ping_pong do
+  live_loop :right do
     sleep 0.5
     play_pattern_timed [74,74],[0.5]
   end
 end
+sleep 6
+
+use_synth :subpulse
+
+with_fx(:echo) do
+  2.times do
+    play_pattern_timed [62, 65, 58,57],[1.5], cutoff: rrand(40, 100), amp: 0.5, attack: 0, cutoff_max: 110
+  end
+end
+sleep 6
+with_fx(:echo) do
+  2.times do
+    play_pattern_timed [62, 65, 58,57],[1.5], cutoff: rrand(40, 100), amp: 0.5, attack: 0, cutoff_max: 110
+  end
+end
+
+# Coded by Rodrigo Cortez
+# http://roicort.github.io/
+
